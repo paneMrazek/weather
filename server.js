5d35bd48f5d98da00ba7cc629dbe52b57da8b96f
Alex Mills <root@ip-172-31-31-128.us-west-2.compute.internal>
2015-03-12 20:17:59 +0000

    Finished lab 6, adding mongo db. Updated to have comments

--- server.js
+++ server.js
@@ -1,12 +1,43 @@
 var http = require("http"), fs = require("fs");
 var port = 8080;
-var ROOT_DIR = "/home/ec2-user/code/html";
+var ROOT_DIR = "html";
 var url = require("url");
+var MongoClient = require('mongodb').MongoClient;
 
 http.createServer(function(req,res){
 	var urlObj = url.parse(req.url,true, false);
-	
-	if(urlObj.pathname.indexOf("getcity")!=-1){
+	if(urlObj.pathname.indexOf("comment")!=-1){
+		if(req.method === "POST"){
+			var jsonData = "";
+			req.on('data', function(chunk){
+				jsonData+= chunk;
+			});
+			req.on('end',function(){
+				var reqObj = JSON.parse(jsonData);
+				MongoClient.connect('mongodb://localhost/weather', function(err, db){
+					if(err) throw err;
+					db.collection('comments').insert(reqObj,function(err, records){
+						res.writeHead(200);
+						res.end("");
+					});
+				});
+			});
+		}
+		if(req.method === "GET"){
+			MongoClient.connect('mongodb://localhost/weather', function(err, db){
+				if(err) throw err;
+				db.collection("comments", function(err, comments){
+					if(err) throw err;
+					comments.find(function(err,items){
+						items.toArray(function(err, itemArr){
+							res.writeHead(200);
+							res.end(JSON.stringify(itemArr));
+						});
+					});
+				});
+			});
+		}
+	}else if(urlObj.pathname.indexOf("getcity")!=-1){
 		fs.readFile(ROOT_DIR + "/cities.dat.txt", function(err, data){
 			if(err) throw err;
 			var myRe = new RegExp("^"+urlObj.query['q']);
